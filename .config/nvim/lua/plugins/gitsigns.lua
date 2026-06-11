6d7c7a40be27569e6d8070239de2b0ff3d858267
Tejas Singh <tejastsingh@google.com>
2026-06-11 04:49:02 +0000

    Enable gitsigns.nvim Git blame

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,12 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require('gitsigns').setup()
+    require('gitsigns').setup({
+      current_line_blame = false,
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        vim.keymap.set('n', '<leader>hb', gs.blame_line, { buffer = bufnr, desc = 'Blame Line' })
+      end
+    })
   end,
 }
