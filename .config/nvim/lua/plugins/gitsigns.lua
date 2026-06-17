47487a6dab321e18e8e8e3823860c51e23748423
Tejas Singh <tejastsingh@google.com>
2026-06-17 08:36:28 +0000

    Add keybinds for git hunk preview

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,9 +5,10 @@ return {
       current_line_blame = false,
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
-        vim.keymap.set('n', ']h', function() gs.nav_hunk('next') end, { buffer = bufnr, desc = 'Next Hunk' })
-        vim.keymap.set('n', '[h', function() gs.nav_hunk('prev') end, { buffer = bufnr, desc = 'Prev Hunk' })
-        vim.keymap.set('n', '<leader>hb', gs.blame_line, { buffer = bufnr, desc = 'Blame Line' })
+        vim.keymap.set('n', '<leader>bl', gs.blame_line, { buffer = bufnr, desc = 'Blame Line' })
+        vim.keymap.set('n', ']h', gs.next_hunk, { buffer = bufnr, desc = 'Next Hunk' })
+        vim.keymap.set('n', '[h', gs.prev_hunk, { buffer = bufnr, desc = 'Prev Hunk' })
+        vim.keymap.set('n', '<leader>ph', gs.preview_hunk_inline, { buffer = bufnr, desc = 'Preview Hunk' })
       end
     })
   end,
