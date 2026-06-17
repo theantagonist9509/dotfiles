68f7a737e88746c0ac7921d0077e2e7accc43b9a
Tejas Singh <tejastsingh@google.com>
2026-06-17 03:15:30 +0000

    Add keybinds to cycle through git hunks

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,6 +5,8 @@ return {
       current_line_blame = false,
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
+        vim.keymap.set('n', ']h', function() gs.nav_hunk('next') end, { buffer = bufnr, desc = 'Next Hunk' })
+        vim.keymap.set('n', '[h', function() gs.nav_hunk('prev') end, { buffer = bufnr, desc = 'Prev Hunk' })
         vim.keymap.set('n', '<leader>hb', gs.blame_line, { buffer = bufnr, desc = 'Blame Line' })
       end
     })
